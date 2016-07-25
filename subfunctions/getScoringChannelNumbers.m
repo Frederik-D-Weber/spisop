401e8f5ead20fcb0f625314150e46296e17ac328
Frederik D. Weber <research@frederikweber.com>
2016-07-25 14:48:48 +0200

    fixed channel selection if no eog or emg channels is in the data

--- subfunctions/getScoringChannelNumbers.m
+++ subfunctions/getScoringChannelNumbers.m
@@ -1,5 +1,5 @@
 function [numberEEG numberEOG numberEMG] = getScoringChannelNumbers(datachannel)
-
+% datachannel = data.label
 eeg_channel = ft_channelselection('*C3*',datachannel);
 eog_channel = ft_channelselection('*EOG*',datachannel);
 emg_channel = ft_channelselection('*EMG*',datachannel);
@@ -13,13 +13,13 @@ end
 if ~isempty(eog_channel)
     numberEOG = find(strcmp(eog_channel(1),datachannel));
 else
-    numberEEG = 1;
+    numberEOG = 1;
 end
 
 if ~isempty(emg_channel)
     numberEMG = find(strcmp(emg_channel(1),datachannel));
 else
-    numberEEG = numel(datachannel);
+    numberEMG = numel(datachannel);
 end
 
 end
